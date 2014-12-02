dc8c0d3e7e99fe14d2c9a330d0deff858d0f13b7
Nathan Lee <nathan@globalphobia.com>
2014-12-01 23:00:51 -0700

    first real test

--- spec/yardstick_spec.rb
+++ spec/yardstick_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe Guard::Yardstick do
+  subject(:guard) { Guard::Yardstick.new(options) }
+  let(:options) { {} }
+
+  describe '#options' do
+    subject { super().options }
+
+    context 'by default' do
+      let(:options) { {} }
+
+      describe '[:all_on_start]' do
+        subject { super()[:all_on_start] }
+        it { should be_truthy }
+      end
+    end
+  end
 end
